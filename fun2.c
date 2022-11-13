229244e9ea27400a142f943e7c98a482d2024a66
Abhishek Dagar <89510493+abhishek-dagar2531@users.noreply.github.com>
2022-11-13 17:24:52 +0530

    Add files via upload

---
+++ fun2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void namaste();
+void bonjour();
+int main(){
+printf("enter f for french  &  i for indian\n");
+char ch;
+scanf("%c",&ch);
+
+if(ch == 'i') {
+     namaste();
+}
+else{
+    bonjour();
+}
+return 0;
+}
+void namaste(){
+    printf("namaste\n");
+}
+void bonjour(){
+    printf("bonjour\n");
+}
