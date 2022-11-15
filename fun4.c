45249f1d3a76fecee5c53bfce8b854bec5a5bfae
Abhishek Dagar <89510493+abhishek-dagar2531@users.noreply.github.com>
2022-11-15 21:55:45 +0530

    Add files via upload

---
+++ fun4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main(){
+
+    int n,c;
+    printf("enter the number:");
+    scanf("%d",&n);
+    c=pow(n,2);
+    printf(" the square is :%d",c);
+    return 0;
+
+}
